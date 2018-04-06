2bb8ada8d192fc0c49de4c20b606e8a90f97f5b4
art-alvarezj <julian.alvarez@articulacion.com.co>
2018-04-06 11:11:52 -0500

    Actualizacion PIM Relaciones Clases

---
+++ ValidadorEcat/validadorEcat.bat
@@ -0,0 +1,3 @@
+set CURRENT_DIR=%cd%
+set CLASSPATH="%CURRENT_DIR%\lib\swing-layout.jar";"%CURRENT_DIR%\lib\swing-layout.jar";"%CURRENT_DIR%\lib\dom.jar";"%CURRENT_DIR%\lib\sax.jar";"%CURRENT_DIR%\lib\jaxp-api.jar"
+java    -classpath %CLASSPATH%;.   com.soluziona.fidufosyga.gui.MainAplication 3.0 3.0
